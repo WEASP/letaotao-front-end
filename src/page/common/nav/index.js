a266ae159e5d25861df977ed2ef3ee3987027dd2
hellojackhui <HUIJIAWEI@hotmail.com>
2017-08-31 09:39:26 +0800

    个人模块开发完成

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Created by JackHui on 2017/8/2.
  */
 'use strict';
@@ -6,7 +6,7 @@ require('./index.css');
 
 var _ltt        =require('util/ltt.js');
 var _user       =require('service/user-service.js');
-
+var _cart       =require('service/cart-service.js');
 //导航
 var nav= {
     init : function () {
